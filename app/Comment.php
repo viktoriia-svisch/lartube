6bf03a469691d3cbfdacde628fcc407355777df5
Viktoriia <viktoriia.svisch@devit-team.com>
2021-01-03 14:51:30 +0000

    add comment-system

--- app/Comment.php
+++ app/Comment.php
@@ -7,4 +7,7 @@ class Comment extends Model
     protected $fillable = [
         'id','users_id', 'medias_id', 'body'
     ];
+    public function user() {
+      return User::find($this->users_id);
+    }
 }
