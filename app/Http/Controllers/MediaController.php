5b42a605017fdd3aaebd5f8304403dd59b2df23d
Viktoriia <viktoriia.svisch@devit-team.com>
2021-01-06 06:18:36 +0000

    some more use of tags

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -27,6 +27,7 @@ class MediaController extends Controller
           $posterPath = $posterFile->store('public/media/posters');
         }
         $media = Media::create(['title' =>  $request->input('title'),'source' => $request->input('source'),'poster_source' => $posterFile, 'description' => $request->input('description'), 'type' => $request->input('type'), 'users_id' => Auth::id()]);
+        $media->retag(explode(' ', $request->input('tags')));
         return redirect()->route('medias.add')
                         ->with('success','Video created successfully');
     }
@@ -43,6 +44,7 @@ class MediaController extends Controller
           $posterPath = $posterFile->store('public/media/posters');
         }
         $media = Media::create(['title' => $title,'source' => $path,'poster_source' => $posterPath,'type' => 'localVideo', 'description' => $request->input('description'), 'users_id' => Auth::id()]);
+        $media->retag(explode(' ', $request->input('tags')));
         return redirect()->route('medias.add')
                         ->with('success','Video created successfully');
       }
@@ -50,6 +52,7 @@ class MediaController extends Controller
         $path = $file->store('public/directMedia');
         $posterPath = $posterFile->store('public/media/posters');
         $media = Media::create(['title' => $title,'source' => $path,'poster_source' => $posterPath,'type' => 'localAudio', 'description' => $request->input('description'), 'users_id' => Auth::id()]);
+        $media->retag(explode(' ', $request->input('tags')));
         return redirect()->route('medias.add')
                         ->with('success','Audio created successfully');
       } else {
@@ -72,6 +75,7 @@ class MediaController extends Controller
         $media->title = $request->input('title');
         $media->source = $request->input('source');
         $media->description = $request->input('description');
+        $media->retag(explode(' ', $request->input('tags')));
         if(!empty($request->input('type'))){
           $media->type = $request->input('type');
         }
