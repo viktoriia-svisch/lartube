4f9fa725c64201f9b3edc01172fee8f7f36b33ba
Viktoriia <viktoriia.svisch@devit-team.com>
2021-04-18 16:47:01 +0000

    make carousel working in vue

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -58,8 +58,7 @@
                 margin-bottom: 30px;
             }
         </style>
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    </head>
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet"></head>
     <body>
       <div id="app">
   <exco :aSubFirst="aFirst"></exco>
