0706b1b92dd8a4a931c1edba1f6b68fab8a050ba
Viktoriia <viktoriia.svisch@devit-team.com>
2022-06-14 00:17:29 +0000

    extend about

--- resources/assets/js/components/About.vue
+++ resources/assets/js/components/About.vue
@@ -1,24 +1,22 @@
 <template>
   <div>
-<h2>About LaraTube</h2>
-<p>LaraTube was created because of security-ascpects, for develop a own, indipendend project and finaly: To learn Laravel and Vue.</p>
-<p>It is a work in progress, a pre-alpha!</p>
-<h3>Credits</h3>
-<p>
-  This project uses a lot of ressources to be created. Thanks to all of them!
-  <ul>
-  <li>Laravel</li>
-  <li>Vue</li>
-  <li>VueSax</li>
-  <li>Bootstrap vue</li>
-  <li>Stackoverflow</li>
-  <li>ApexCharts</li>
-  <li>Webtorrent</li>
-  <li>hootlex/laravel-friendships</li>
-  <li>james-heinrich/getid3</li>
-  <li>rtconner/laravel-tagging</li>
-  <li>spatie/laravel-permission</li>
-</ul>
-</p>
-</div>
+    <h2>About LaraTube</h2>
+    <p>LaraTube was created because of security-ascpects, for develop a own, indipendend project and finaly: To learn Laravel and Vue.</p>
+    <p>It is a work in progress, a pre-alpha!</p>
+    <h3>Gitlab</h3>
+    <p>Check out the project's source on 
+      <a href="https:    </p>
+    <p>Read the Readme.md-file there for more information</p>
+    <h2>Credits</h2>
+    <p>
+      This project uses a lot of ressources to be created. Thanks to all of them!
+      <ul>
+        <li><a href="https:        <li><a href="https:        <li><a href="https:        <li><a href="https:        <li><a href="https:        <li>ApexCharts</li>
+        <li><a href="https:        <li>hootlex/laravel-friendships</li>
+        <li>james-heinrich/getid3</li>
+        <li>rtconner/laravel-tagging</li>
+        <li>spatie/laravel-permission</li>
+      </ul>
+    </p>
+  </div>
 </template>
